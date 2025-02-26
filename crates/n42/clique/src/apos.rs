55b83ae918eb5aa50296268fd0ca0c4d2c5c63fd
build-debug <191841866+build-debug@users.noreply.github.com>
2025-02-26 22:02:35 +0800

    chore: clean error description

--- crates/n42/clique/src/apos.rs
+++ crates/n42/clique/src/apos.rs
@@ -202,7 +202,7 @@ where
         //Analyze the signer and check if they are in the signer list
         let signer = recover_address(header)?;
         if !snap.signers.contains(&signer) {
-            info!(target: "consensus::apos", "err signer: {}", signer);
+            info!(target: "consensus::apos", "err signer not in list: {}", signer);
             return Err(AposError::UnauthorizedSigner.into());
         }
         info!(target: "consensus::apos", "recovered address: {}", signer);
@@ -604,7 +604,7 @@ Some(vec![parent.header().clone()]))?;
         // Bail out if we're unauthorized to sign a block
         let snap = self.snapshot(header.number - 1, header.parent_hash, None)?;
         if !snap.signers.contains(&signer) {
-            error!(target: "consensus::pos", "err signer: {}", signer);
+            error!(target: "consensus::pos", "err signer not in list: {}", signer);
             return Err(ConsensusError::UnauthorizedSigner)
         }
 

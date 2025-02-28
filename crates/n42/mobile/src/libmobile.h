7f26102f133c097c29cffa28492807ac4a059be4
SCslope <2292633789@qq.com>
2025-02-28 15:43:14 +0800

    ly: add crates/n42/mobile

---
+++ crates/n42/mobile/src/libmobile.h
@@ -0,0 +1,16 @@
+// // my_library.h
+// #ifndef MY_LIBRARY_H
+// #define MY_LIBRARY_H
+
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+
+// // 函数声明
+// void evmTest(long value);
+
+// #ifdef __cplusplus
+// }
+// #endif
+
+// #endif // MY_LIBRARY_H

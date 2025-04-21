555849c2f3d2306bdfdeccd44f3888e5dd8fe3ab
build-debug <191841866+build-debug@users.noreply.github.com>
2025-04-21 10:35:08 +0800

    fix: fix more hardforks(from reth dev)

--- crates/ethereum-forks/src/hardfork/ethereum.rs
+++ crates/ethereum-forks/src/hardfork/ethereum.rs
@@ -399,20 +399,13 @@ impl EthereumHardfork {
             (Self::London, ForkCondition::Block(0)),
             (Self::ArrowGlacier, ForkCondition::Block(0)),
             (Self::BeiJing, ForkCondition::Block(0)),
-            (Self::Paris, ForkCondition::Block(0)),
-            /*
             (
                 Self::Paris,
                 ForkCondition::TTD {
-                    fork_block: None,
-                    total_difficulty: uint!(58_750_000_000_000_000_000_000_U256),
+                    fork_block: Some(0),
+                    total_difficulty: U256::ZERO,
                 },
             ),
-            */
-            /*
-            (Self::Shanghai, ForkCondition::Block(0)),
-            (Self::Cancun, ForkCondition::Block(0)),
-            */
             (Self::Shanghai, ForkCondition::Timestamp(1681338455)),
             (Self::Cancun, ForkCondition::Timestamp(1710338135)),
         ]

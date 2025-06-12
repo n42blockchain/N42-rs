41033fd655acfa483f8d780bf0352cb516b8af3b
build-debug <191841866+build-debug@users.noreply.github.com>
2025-06-12 17:57:43 +0800

    chore: remove code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -1135,7 +1135,6 @@ async fn test_ensure_that_pending_votes_dont_survive_authorization_status_change
                 signer: "D".to_string(),
                 voted: Some("F".to_string()),
                 auth: Some(true),
-                ..Default::default()
             },
             TesterVote {
                 signer: "E".to_string(),

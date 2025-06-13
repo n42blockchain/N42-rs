c1ba0212bc161249f91782dc64a9999e3fe9b6c8
build-debug <191841866+build-debug@users.noreply.github.com>
2025-06-13 18:38:18 +0800

    chore: remove code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -314,7 +314,6 @@ async fn test_two_signers__voting_to_add_three_others__only_accept_first_two__th
                 signer: "B".to_string(),
                 voted: Some("C".to_string()),
                 auth: Some(true),
-                ..Default::default()
             },
             TesterVote {
                 signer: "A".to_string(),

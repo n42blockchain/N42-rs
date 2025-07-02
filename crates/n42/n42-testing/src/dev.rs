5189095db624f3898c4e0c082bc036e1301ae73d
build-debug <191841866+build-debug@users.noreply.github.com>
2025-07-02 18:48:48 +0800

    chore: clean code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -1089,7 +1089,6 @@ async fn test_ensure_that_pending_votes_dont_survive_authorization_status_change
                 signer: "A".to_string(),
                 voted: Some("F".to_string()),
                 auth: Some(true),
-                ..Default::default()
             },
             TesterVote {
                 signer: "B".to_string(),

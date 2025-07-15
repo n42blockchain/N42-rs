f0d9002ebb0c9f03840d984d7dd729bf919b3149
build-debug <191841866+build-debug@users.noreply.github.com>
2025-07-15 18:44:59 +0800

    chore: clean code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -967,7 +967,6 @@ async fn test_changes_reaching_consensus_out_of_bounds__via_a_deauth__execute_on
                 signer: "B".to_string(),
                 voted: Some("D".to_string()),
                 auth: Some(false),
-                ..Default::default()
             },
             TesterVote {
                 signer: "C".to_string(),

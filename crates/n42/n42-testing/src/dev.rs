178e8a1def0b6815a755492d69de3199f996b86a
build-debug <191841866+build-debug@users.noreply.github.com>
2025-07-24 18:17:46 +0800

    chore: clean code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -879,7 +879,6 @@ async fn test_cascading_changes_are_not_allowed__only_the_account_being_voted_on
                 signer: "A".to_string(),
                 voted: Some("D".to_string()),
                 auth: Some(false),
-                ..Default::default()
             },
             TesterVote {
                 signer: "B".to_string(),

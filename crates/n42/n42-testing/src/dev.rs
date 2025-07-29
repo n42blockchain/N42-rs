4c48c5eaa89b7fa22ca276a6f4abc5d41b5be4e6
build-debug <191841866+build-debug@users.noreply.github.com>
2025-07-29 19:10:26 +0800

    chore: clean code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -832,7 +832,6 @@ async fn test_votes_from_deauthorized_signers_are_discarded_immediately__auth_vo
                 signer: "B".to_string(),
                 voted: Some("C".to_string()),
                 auth: Some(false),
-                ..Default::default()
             },
             TesterVote {
                 signer: "A".to_string(),

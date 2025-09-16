3178d6c89621236f1211ec9ff714c9e691ff4919
build-debug <191841866+build-debug@users.noreply.github.com>
2025-09-16 18:35:08 +0800

    chore: clean code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -131,7 +131,7 @@ async fn new_block<Node: FullNodeComponents, AddOns: RethRpcAddOns<Node>>(node: 
         ForkchoiceState {
             head_block_hash: new_head,
             safe_block_hash: current_head,
-                finalized_block_hash: current_head,
+            finalized_block_hash: current_head,
             },
             None,
         ).await?;

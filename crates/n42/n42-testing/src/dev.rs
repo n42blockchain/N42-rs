bd6fe98342a22e0b95eba10334aaa9c0768f8d81
build-debug <191841866+build-debug@users.noreply.github.com>
2025-10-28 18:20:22 +0800

    chore: clean code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -6,7 +6,8 @@ use reth_primitives_traits::{
     NodePrimitives
 };
 use reth_chainspec::make_genesis_header;
-use alloy_primitives::{Sealable, FixedBytes};
+use alloy_primitives::{
+    Sealable, FixedBytes};
 use reth_node_builder::node::NodeTypes;
 use reth_payload_builder::EthPayloadBuilderAttributes;
 use reth_ethereum_forks::N42_HARDFORKS_FOR_CLIQUE_TEST;

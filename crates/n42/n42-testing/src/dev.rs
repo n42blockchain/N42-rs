a5035a640eccb64da615ea043b3b551b478c9379
build-debug <191841866+build-debug@users.noreply.github.com>
2025-11-05 18:19:43 +0800

    chore: clean code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -21,7 +21,8 @@ use reth_payload_primitives::{
 use reth_consensus::Consensus;
 use reth_node_api::{
     FullNodeComponents,
-    FullNodeTypes, PayloadTypes, EngineTypes};
+    FullNodeTypes,
+    PayloadTypes, EngineTypes};
 use zerocopy::AsBytes;
 use reth_chainspec::{ChainSpec, N42};
 use reth_provider::{BlockHashReader, BlockReaderIdExt, BlockNumReader};

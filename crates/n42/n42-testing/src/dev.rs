a0266092f2a8e9679feba5795d0452e0bbaefd42
build-debug <191841866+build-debug@users.noreply.github.com>
2025-12-09 18:19:20 +0800

    chore: clean code

--- crates/n42/n42-testing/src/dev.rs
+++ crates/n42/n42-testing/src/dev.rs
@@ -55,7 +55,8 @@ use reth::{
 use reth_node_builder::{
     NodeBuilder,
     NodeConfig,
-    NodeHandle, FullNode, rpc::RethRpcAddOns,
+    NodeHandle,
+    FullNode, rpc::RethRpcAddOns,
 };
 use reth_tasks::TaskManager;
 use n42_engine_types::N42Node;

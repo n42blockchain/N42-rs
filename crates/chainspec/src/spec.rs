f225320e691710a202cb4cc12f09a2e5f35c528f
Nyxen <1020080523@qq.com>
2026-01-19 14:32:12 -0500

    fix: Add blob_base_cost field to BlobParams in chainspec tests
    Add missing blob_base_cost field (default value 0) to BlobParams initializations in blob_params_from_genesis test. This field was added in alloy-eips v1.4.3 as part of EIP-7840.

--- crates/chainspec/src/spec.rs
+++ crates/chainspec/src/spec.rs
@@ -3411,6 +3411,7 @@ Post-merge hard forks (timestamp based):
                 max_blobs_per_tx: 6,
                 update_fraction: 3338477,
                 min_blob_fee: BLOB_TX_MIN_BLOB_GASPRICE,
+                blob_base_cost: 0,
             },
             prague: BlobParams {
                 target_blob_count: 3,
@@ -3418,6 +3419,7 @@ Post-merge hard forks (timestamp based):
                 max_blobs_per_tx: 6,
                 update_fraction: 3338477,
                 min_blob_fee: BLOB_TX_MIN_BLOB_GASPRICE,
+                blob_base_cost: 0,
             },
             ..Default::default()
         };
